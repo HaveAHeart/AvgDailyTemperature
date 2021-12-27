ce5c3497407d7fc92a3c43aaf9478d5dc87f885c
Alexey <bedrin18@mail.ru>
2021-12-27 19:06:25 +0300

    Revert "Merge pull request #18 from HaveAHeart/develop"
    This reverts commit 64c01721ee0e1f1f77fed0f67969ece3f10c6413, reversing
    changes made to f356b756eeac42c981323cf417d5e6a933fc982c.

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ Small app to observe average daily temperature.
 2. Run an interactive container with network access (docker run -it --network host)
 3. Run commands and get the temperature! (--help for list of commands)
 
-
-
 ##Usage examples:
 - Moscow : Average temperature for Moscow, 2021-11-27 is 0.6C / 33.2F.
 - --date 2021-11-26 : currently set date is 2021-11-26
--- main.py
+++ main.py
@@ -2,101 +2,86 @@ import requests
 from datetime import datetime
 import re
 
+# Specially generated API key for (mostly) public usage with https://weatherapi.com/
+savedAPIKey = '3fc69af2efcb4a929b7112443212711'
+apiKey = '3fc69af2efcb4a929b7112443212711'
 
-class WeatherApp:
-    startMsg = 'Type --help or list of commands.'
-    helpMsg = 'Short help: type city/country name for avg temperature (WITH DASHES, NOT SPACES):\n' \
-              'e.g. "Saint-Petersburg" or "USA"\n' \
-              '--exit to exit the app\n' \
-              '--help for list of commands\n' \
-              '--apikey HERE_API_KEY to change the api key or --apikey default to reset it.\n' \
-              '--date YYYY-mm-dd to set the date, or --date default to reset to current one (WORKS ONLY FOR LAST WEEK).'
-    exitMsg = 'Bye!'
+startMsg = 'Type --help or list of commands.'
+helpMsg = 'Short help: type city/country name for avg temperature (WITH DASHES, NOT SPACES):\n' \
+          'e.g. "Saint-Petersburg" or "USA"\n' \
+          '--exit to exit the app\n' \
+          '--help for list of commands\n' \
+          '--apikey HERE_API_KEY to change the api key or --apikey default to reset it.\n' \
+          '--date YYYY-mm-dd to set the date, or --date default to reset to current one (WORKS ONLY WITH LAST WEEK).'
+exitMsg = 'Bye!'
 
-    # Specially generated API key for (mostly) public usage with https://weatherapi.com/
+y = datetime.now().year
+m = datetime.now().month
+d = datetime.now().day
 
-    def __init__(self):
-        self.savedAPIKey = '3fc69af2efcb4a929b7112443212711'
-        self.apiKey = '3fc69af2efcb4a929b7112443212711'
-        self.y = datetime.now().year
-        self.m = datetime.now().month
-        self.d = datetime.now().day
-        self.args = []
 
-    def getWeatherData(self, city):
-        url = 'https://api.weatherapi.com/v1/history.json?key={}&q={}&dt={}-{}-{}'
-        resUrl = url.format(self.apiKey, city, self.y, self.m, self.d)
-        response = requests.get(resUrl)
-        return response
-
-    def setApiKey(self, arg):
-        if arg == 'default':
-            self.apiKey = self.savedAPIKey
-        else:
-            self.apiKey = arg
-
-    def setDate(self, arg):
-        if arg == 'default':
-            self.y = datetime.now().year
-            self.m = datetime.now().month
-            self.d = datetime.now().day
-        else:
-            if re.match(r'^\d\d\d\d-\d\d-\d\d$', arg):
-                date = arg.split("-")
-                self.y = date[0]
-                self.m = date[1]
-                self.d = date[2]
-            else:
-                print("Incorrect date format (should be yyyy-mm-dd).")
-
-    def parseIncomingData(self, city):
-        result = self.getWeatherData(city)
-        if result.status_code == 200:
-            dailyRes = result.json().get('forecast').get('forecastday')[0].get('day')
-            avgC = dailyRes.get('avgtemp_c')
-            avgF = dailyRes.get('avgtemp_f')
-            self.printIncomingData(city, self.y, self.m, self.d, avgC, avgF)
-        elif result.status_code == 400:
-            print("bad request error. trying to ruin the request, huh?")
-        elif result.status_code == 401:
-            print('unauthorised error. Looks like api key is depleted - try setting yours with --apikey.')
-        elif result.status_code == 500:
-            print('internal server error. Looks like server is down. Try again later.')
-        else:
-            print('the error has occured for city {}. Maybe, try --help?'.format(self.args[0]))
-        return result.status_code
-
-    def printIncomingData(self, city, year, month, day, avgC, avgF):
-        print('Average temperature for {}, {}-{}-{} is {}C / {}F.'.format(city, year, month, day, avgC, avgF))
-
-    def run(self):
-        print(self.startMsg)
-        while True:
-            self.args = input().split()
-            if len(self.args) == 0:
-                print('Hey, type something')
-            elif len(self.args) == 1:
-                if (self.args[0]) == '--help':
-                    print(self.helpMsg)
-                elif (self.args[0]) == '--exit':
-                    print(self.exitMsg)
-                    break
-                else:
-                    self.parseIncomingData(self.args[0])
-            elif len(self.args) == 2:
-                if (self.args[0]) == '--apikey':
-                    self.setApiKey(self.args[1])
-                    print('apikey is now {}'.format(self.apiKey))
-                elif (self.args[0]) == '--date':
-                    self.setDate(self.args[1])
-                    print('currently set date is {}-{}-{}'.format(self.y, self.m, self.d))
-
-                else:
-                    print('Incorrect command. Maybe, try --help?')
-            else:
-                print('Only one city at a time, please. Also do not forget to use "-" (dash) instead of " " (space)')
+def getWeatherData(city, year, month, day):
+    url = 'https://api.weatherapi.com/v1/history.json?key={}&q={}&dt={}-{}-{}'
+    resUrl = url.format(apiKey, city, year, month, day)
+    response = requests.get(resUrl)
+    return response
 
 
 if __name__ == '__main__':
-    wApp = WeatherApp()
-    wApp.run()
+    print(startMsg)
+    while True:
+        args = input().split()
+        if len(args) == 0:
+            print('Hey, type something')
+        elif len(args) == 1:
+            if (args[0]) == '--help':
+                print(helpMsg)
+            elif (args[0]) == '--exit':
+                print(exitMsg)
+                break
+            else:
+                res = getWeatherData(args[0], y, m, d)
+                if res.status_code == 200:
+                    dailyRes = res.json().get('forecast').get('forecastday')[0].get('day')
+                    avgC = dailyRes.get('avgtemp_c')
+                    avgF = dailyRes.get('avgtemp_f')
+                    print('Average temperature for {}, {}-{}-{} is {}C / {}F.'.format(args[0], y, m, d, avgC, avgF))
+                elif res.status_code == 400:
+                    print("bad request error. trying to ruin the request, huh?")
+                elif res.status_code == 401:
+                    print('unauthorised error. Looks like api key is depleted - try setting yours with --apikey.')
+                elif res.status_code == 500:
+                    print('internal server error. Looks like server is down. Try again later.')
+                else:
+
+                    print('the error has occured for city {}. Maybe, try --help?'.format(args[0]))
+        elif len(args) == 2:
+            if (args[0]) == '--apikey':
+                if args[1] == 'default':
+                    apiKey = savedAPIKey
+                else:
+                    apiKey = args[1]
+                print('apikey is now {}'.format(apiKey))
+            elif (args[0]) == '--date':
+                if (args[1]) == 'default':
+                    y = datetime.now().year
+                    m = datetime.now().month
+                    d = datetime.now().day
+                else:
+                    if re.match('^\d\d\d\d-\d\d-\d\d$', args[1]):
+                        userDate = args[1].split("-")
+                        y = userDate[0]
+                        m = userDate[1]
+                        d = userDate[2]
+                    else:
+                        print("Incorrect date format (should be yyyy-mm-dd).")
+                print('currently set date is {}-{}-{}'.format(y, m, d))
+
+            else:
+                print('Incorrect command. Maybe, try --help?')
+        else:
+            print('Only one city at a time, please. Also do not forget to use "-" (dash) instead of " " (space)')
+
+
+
+
